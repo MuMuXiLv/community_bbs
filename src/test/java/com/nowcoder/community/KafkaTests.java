a47de0895fbca1bf5320b7b0595aacc141501184
MuMuXiLv <M202072857@hust.edu.cn>
2022-07-15 17:10:43 +0800

    还剩最后优化

---
+++ src/test/java/com/nowcoder/community/KafkaTests.java
@@ -0,0 +1,58 @@
+package com.nowcoder.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//必须要启动kafka和zookeeper服务器才能使用
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void testKafka() {
+        kafkaProducer.sendMessage("test", "你好");
+        kafkaProducer.sendMessage("test", "在吗");
+
+        try {
+            Thread.sleep(1000 * 20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+@Component
+class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
+    }
+
+}
+
+@Component
+class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"})
+    public void handleMessage(ConsumerRecord record) {
+        System.out.println(record.value());
+    }
+
+
+}
